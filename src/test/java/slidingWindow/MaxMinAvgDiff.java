37e4c1406caa47649bbdee7ea2c16b7d7f9440dc
Manish Singh <prataponandroid@gmail.com>
2024-05-08 08:15:05 +0200

    refactored

--- src/test/java/slidingWindow/MaxMinAvgDiff.java
+++ src/test/java/slidingWindow/MaxMinAvgDiff.java
@@ -1,5 +1,6 @@
 package slidingWindow;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -7,14 +8,12 @@ import org.junit.jupiter.api.Test;
  * Output: 6.5
  * All subarrays of length 2 are {3, 8}, {8, 9}, {9, 15} and their averages are
  * (3+8)/2 = 5.5, (8+9)/2 = 8.5, and (9+15)/2 = 12.0 respectively.
- * Therefore, the difference between the maximum(=12.0) and minimum(=5.5) is 12.0 -5.5 = 6.5.
+ * Therefore, the difference between the maximum(=12.0) and minimum(=5.5) is 12.0 - 5.5 = 6.5.
  */
 public class MaxMinAvgDiff {
 
-    @Test
-    public void test() {
-        int[] arr = {3, 8, 9, 15};
-        int k = 2;
+    private float test(int[] arr, int k) {
+
         float minAvg, maxAvg;
         // sum
         int total = 0;
@@ -23,11 +22,18 @@ public class MaxMinAvgDiff {
         }
         minAvg = maxAvg = total / 2f;
 
-        for (int i = 1; i < arr.length-1; i++) {
-            total = total - arr[i - 1] + arr[i + 1];
+        for (int i = k; i < arr.length; i++) {
+            total = total - arr[i - k] + arr[i];
             minAvg = Math.min(minAvg, total / 2f);
             maxAvg = Math.max(maxAvg, total / 2f);
         }
-        System.out.println(maxAvg - minAvg);
+        return maxAvg - minAvg;
+    }
+
+    @Test
+    public void test() {
+        float diff = new MaxMinAvgDiff().test(new int[]{3, 8, 9, 15}, 2);
+        System.out.println(diff);
+        Assertions.assertEquals(6.5, diff);
     }
 }
